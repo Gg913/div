4bf93a601e36cd0d88f2e5ba32dea54a61b7112d
Gg913 <89768562+Gg913@users.noreply.github.com>
2022-10-26 23:11:52 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,122 +1,120 @@
-const fs = require("fs");
+const fs = require('fs')
+var colors = require('colors')
+
+const {status_do_bot, log_removeu_o_bot, log_entrou_no_servidor, id_do_servidor} = require('./logs.json')
+
 console.clear()
-console.log ("Ligando O Bot De Divulgação...")
-const Discord = require("discord.js");
-const client = new Discord.Client({
-  autoReconnect: true,
-  messageCacheMaxSize: 2024,
-  fetchAllMembers: true,
-  disabledEvents: ["typingStart", "typingStop", "guildMemberSpeaking"],
-  messageCacheLifetime: 1680,
-  messageSweepInterval: 1680
-});
-const config = require("./config.json");
-const { Client, Util } = require("discord.js");
-const { green } = require("chalk");
-var token = config.token;
-var prefix = config.prefix;
-var dono = config.dono;
 
-client.login(token);
-process.title = "Conectando...";
+const Discord = require('discord.js'),
+	client = new Discord.Client({
+		autoReconnect: true,
+		messageCacheMaxSize: 2024,
+		fetchAllMembers: true,
+		disabledEvents: ['typingStart', 'typingStop', 'guildMemberSpeaking'],
+		messageCacheLifetime: 1680,
+		messageSweepInterval: 1680,
+	}),
+	lofy = require('./lofy.json'),
+	{Client, Util} = require('discord.js'),
+	{green, bgRed} = require('chalk')
 
-client.on("message", message => {
-  if (message.channel.type == "dm") return;
-  if (message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
-  let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
-  let args = message.content.split(" ").slice(1);
-  try {
-    let commandFile = require(`./comandos/${command}.js`);
-    commandFile.run(client, message, args);
-  } catch (err) {
-    if (err.code == "MODULE_NOT_FOUND") return;
-    console.error(err);
-  }
-});
+var token = lofy.token,
+	prefixo = lofy.prefixo,
+	dono = lofy.dono
 
-client.on("ready", () => {
-  process.title = `Conectado em: ${client.user.username}`;
-  console.clear()
-  console.log(
-    ` Σ||Bot Foi Iniciado.\n → Servidores Totais: ${client.guilds.size}.\n → Usuários Totais: ${client.users.size}.\n → Nome:${client.user.username}.\n → Id:${client.user.id}.`);
-   
-    console.log ("")
-console.log(" ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ ")
-console.log(" ░ ██╗░░░██╗██████╗░██████╗░░█████╗░██╗░░██╗███████░  ")
-console.log(" ░╚██╗░██╔╝██╔══██╗██╔══██╗██╔══██╗╚██╗██╔╝╚════██║░ ")
-console.log(" ░░╚████╔╝░██║░░██║██████╔╝██║░░██║░╚███╔╝░░░███╔═╝░ ")
-console.log(" ░░░╚██╔╝░░██║░░██║██╔══██╗██║░░██║░██╔██╗░██╔══╝░░░ ")
-console.log(" ░░░░██║░░░██████╔╝██║░░██║╚█████╔╝██╔╝╚██╗███████╗░ ")
-console.log(" ░░░░╚═╝░░░╚═════╝░╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝╚══════╝░ ")
-console.log(" ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ ")
-    console.log ("")
-    
-  client.user.setPresence({ game: { name: config.Status, type: "PLAYING" } });
+;(process.title = 'Lofy Selfbot V4'),
+	client.login(token).catch((c) => {
+		console.log(''.red), console.log('     → Um token invalido foi usado'.red), console.log(('     → ' + c).red)
+	}),
+	console.clear(),
+	client.on('message', (d) => {
+		if (d.channel.type == 'dm') return
+		if (d.author.bot) return
+		if (!d.content.startsWith(prefixo)) return
+		let aa = d.content.split(' ')[0]
+		aa = aa.slice(prefixo.length)
+		let ab = d.content.split(' ').slice(1)
 
-  let status = [
-    { name: `ydroxz Brabo Das Divs!`, type: "PLAYING" },
-    { name: `Entra ae: https://discord.gg/wmCW5bHSmP`, type: "PLAYING" },
-    { name: `Infinity Store No Topo!`, type: "PLAYING" }
-  ];
+		try {
+			let ac = require('./comandos/' + aa + '.js')
+			ac.run(client, d, ab)
+		} catch (ad) {
+			if (ad.code == 'MODULE_NOT_FOUND') return
 
-  function st() {
-    let rs = status[Math.floor(Math.random() * status.length)];
-    client.user.setPresence({ game: rs });
-  }
-  st();
-  setInterval(() => st(), 10000); //10000 = 10Ms = 10 segundos
-  
-  const ligado = new Discord.RichEmbed()
-    .setAuthor(client.user.username, client.user.avatarURL)
-    .setDescription(
-      `Bot Foi Iniciado.\n\nServidores Totais: ${client.guilds.size}\nUsuários Totais: ${client.users.size}`
-    )
-    .setThumbnail(client.user.avatarURL)
-    .setColor("LUMINOUS_VIVID_PINK");
-  client.guilds
-    .get("719807339690983425")
-    .channels.get("771034681986646037")
-    .send(ligado);
-});
+			console.error(ad)
+		}
+	}),
+	client.on('ready', () => {
+		console.clear(),
+			console.log(
+				`
 
-  client.on("guildCreate", guild => {
-    const nserver = new Discord.RichEmbed()
-      .setTitle(client.user.username)
-      .setDescription(
-        `Entrei Em Um Novo Servidor:\n\nNome: ${guild.name}\nID: ${guild.id}\nMembros: ${guild.memberCount}\nTotal De Servidores: ${client.guilds.size}\nTotal De Membros: ${client.users.size}`
-      )
-      .setThumbnail(
-        guild.iconURL || "https://loritta.website/assets/img/unknown.png"
-      )
-      .setColor("LUMINOUS_VIVID_PINK")
-      .setFooter(client.user.username, client.user.avatarURL)
-      .setTimestamp();
+      ██╗      ██████╗ ███████╗██╗   ██╗    ███████╗███████╗██╗     ███████╗██████╗  ██████╗ ████████╗    ██╗   ██╗██╗  ██╗
+      ██║     ██╔═══██╗██╔════╝╚██╗ ██╔╝    ██╔════╝██╔════╝██║     ██╔════╝██╔══██╗██╔═══██╗╚══██╔══╝    ██║   ██║██║  ██║
+      ██║     ██║   ██║█████╗   ╚████╔╝     ███████╗█████╗  ██║     █████╗  ██████╔╝██║   ██║   ██║       ██║   ██║███████║
+      ██║     ██║   ██║██╔══╝    ╚██╔╝      ╚════██║██╔══╝  ██║     ██╔══╝  ██╔══██╗██║   ██║   ██║       ╚██╗ ██╔╝╚════██║
+      ███████╗╚██████╔╝██║        ██║       ███████║███████╗███████╗██║     ██████╔╝╚██████╔╝   ██║        ╚████╔╝      ██║
+      ╚══════╝ ╚═════╝ ╚═╝        ╚═╝       ╚══════╝╚══════╝╚══════╝╚═╝     ╚═════╝  ╚═════╝    ╚═╝         ╚═══╝       ╚═╝
 
-    client.channels.get("771034759593721876").send(nserver);
-    console.log(
-      `Entrei no servidor ${guild.name}\nMembros: ${guild.memberCount}\nAgora Eu Estou Em: ${client.guilds.size} servidores\nCom Total De Membros: ${client.users.size}`
-    );
-  });
-  client.on("guildDelete", guild => {
-    const nserver = new Discord.RichEmbed()
-      .setTitle(client.user.username)
-      .setDescription(
-        `Fui Removido De Um Servidor:\n\nNome: ${guild.name}\nID: ${guild.id}\nMembros: ${guild.memberCount}\nTotal De Servidores: ${client.guilds.size}\nTotal De Membros: ${client.users.size}`
-      )
-      .setThumbnail(
-        guild.iconURL || "https://loritta.website/assets/img/unknown.png"
-      )
-      .setColor("LUMINOUS_VIVID_PINK")
-      .setFooter(client.user.username, client.user.avatarURL)
-      .setTimestamp();
+`.yellow,
+			)
 
-    client.guilds
-      .get("719807339690983425")
-      .channels.get("771034709248704543")
-      .send(nserver);
-    console.log(
-      `Sai do servidor ${guild.name}\nMembros: ${guild.memberCount}\nAgora Eu Estou Em: ${client.guilds.size} servidores\nCom Total De Membros: ${client.users.size}`
-    );
-});
+		console.log(''),
+			console.log('    → Bot Foi Iniciado.'.blue),
+			console.log(('    → Servidores Totais: ' + client.guilds.size + '.').magenta),
+			console.log(('    → Usuários Totais: ' + client.users.size + '.').blue),
+			console.log(('    → Nome: ' + client.user.username + '.').magenta),
+			console.log(('    → Id: ' + client.user.id + '.').blue),
+			console.log(''),
+			client.user.setPresence({game: {name: lofy.Status, type: 'STREAMING'}})
+
+		let am = [
+			{name: 'Mdz SelfBot', type: 'STREAMING'},
+			{name: 'Rede Mdz', type: 'STREAMING'},
+		]
+
+		function an() {
+			let ao = am[Math.floor(Math.random() * am.length)]
+			client.user.setPresence({
+				game: ao,
+			})
+		}
+
+		an(), setInterval(() => an(), 10000)
+
+		const ap = new Discord.RichEmbed()
+			.setAuthor(client.user.username, client.user.avatarURL)
+			.setDescription('Bot Foi Iniciado\n      Servidores Totais: ' + client.guilds.size + '\n      Usuários Totais: ' + client.users.size)
+			.setThumbnail(client.user.avatarURL)
+			.setColor('LUMINOUS_VIVID_PINK')
+		client.guilds.get(id_do_servidor).channels.get(status_do_bot).send(ap)
+	}),
+	client.on('guildCreate', (aq) => {
+		var as = new Discord.RichEmbed()
+			.setTitle(client.user.username)
+			.setDescription('      → Entrei Em Um Novo Servidor:\n             → Nome: ' + aq.name + '\n             → ID: ' + aq.id + '\n             → Membros: ' + aq.memberCount + '\n             → Total De Servidores: ' + client.guilds.size + '\n             → Total De Membros: ' + client.users.size)
+			.setThumbnail(aq.iconURL || 'https://loritta.website/assets/img/unknown.png')
+			.setColor('LUMINOUS_VIVID_PINK')
+			.setFooter(client.user.username, client.user.avatarURL)
+			.setTimestamp()
+
+		client.channels.get(log_entrou_no_servidor).send(as),
+			console.log(''),
+			console.log(('    → Log Entrada\n    → Entrei no servidor ' + aq.name + '\n    → Membros: ' + aq.memberCount + '\n    → Agora Eu Estou Em: ' + client.guilds.size + ' servidores\n    → Com Total De Membros: ' + client.users.size).grey)
+	}),
+	console.log(''),
+	client.on('guildDelete', (at) => {
+		var av = new Discord.RichEmbed()
+			.setTitle(client.user.username)
+			.setDescription('      → Fui Removido De Um Servidor:\n             → Nome: ' + at.name + '\n             → ID: ' + at.id + '\n             → Membros: ' + at.memberCount + '\n             → Total De Servidores: ' + client.guilds.size + '\n             → Total De Membros: ' + client.users.size)
+			.setThumbnail(at.iconURL || 'https://loritta.website/assets/img/unknown.png')
+			.setColor('LUMINOUS_VIVID_PINK')
+			.setFooter(client.user.username, client.user.avatarURL)
+			.setTimestamp()
+
+		console.log(''),
+			client.guilds.get(id_do_servidor).channels.get(log_removeu_o_bot).send(av),
+			console.log(''),
+			console.log(('    → Log Saida\n    → Sai do servidor ' + at.name + '\n    → Membros: ' + at.memberCount + '\n    → Agora Eu Estou Em: ' + client.guilds.size + ' servidores\n    → Com Total De Membros: ' + client.users.size).red)
+	}),
+	console.log('')
